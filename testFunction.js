869d46dcab3b782b5970d7e1633b4d1bb8695eb7
Kamlesh Kulkarni <91947468+lhtkamleshkulkrni@users.noreply.github.com>
2022-06-14 14:36:31 +0530

    Update testFunction.js

--- testFunction.js
+++ testFunction.js
@@ -2,14 +2,9 @@ const UtilMethods = require("lh-utilities/utilityMethods");
 const BLManager = require("./app/manager");
 const {lhtLog} = require("lh-utilities/utilityMethods");
 
-// Update requestData as per the testing requirement-
-const requestData = {
-    email: 'ayushk@leewayhertz.com',
-    password: 'Password@123'
-}
 
 async function testMethod() {
-    const [error, response] = await UtilMethods.parseResponse(new BLManager().webSocketMonitor(requestData));
+    const [error, response] = await UtilMethods.parseResponse(new BLManager().webSocketMonitor());
     lhtLog('testMethod', 'Success', response);
     lhtLog('testMethod', 'Error', error);
 }
